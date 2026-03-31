ae990cdb839a104d0af1850d03a74639d29c1ec4
tsvetcovoleg-source <tsvetcov.oleg@gmail.com>
2026-03-31 22:27:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 
-print("Start...")
+print("=== START ===")
 
 service_account_json = os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
 sheet_id = os.environ["GOOGLE_SHEET_ID"]
+
+print("Secrets loaded")
 
 info = json.loads(service_account_json)
 
@@ -18,10 +20,16 @@ creds = Credentials.from_service_account_info(
     ],
 )
 
-gc = gspread.authorize(creds)
-sh = gc.open_by_key(sheet_id)
+print("Credentials created")
 
-print("Connected to Google Sheets!")
+gc = gspread.authorize(creds)
+print("gspread authorized")
+
+sh = gc.open_by_key(sheet_id)
+print("Spreadsheet opened")
 
 ws = sh.sheet1
+print("Worksheet title:", ws.title)
 print("First row:", ws.row_values(1))
+
+print("=== DONE ===")
